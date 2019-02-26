5f3f0a59f91040d55e8cab3ac01df1b889e99850
Jagmohan Singh <singhjagmohan4734@gmail.com>
2019-02-25 19:33:03 -0800

    a
    adding middleware

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import List from "../Component/List";
+import Form from "../Component/Form";
 const App = () => (
   <div className="row mt-5">
-    <h2>Articles</h2>
-    <List />
+    <div className="col-md-4 offset-md-1">
+      <h2>Articles</h2>
+      <List />
+    </div>
+    <div className="col-md-4 offset-md-1">
+      <h2>Add new Articles</h2>
+      <Form />
+    </div>
   </div>
 );
 export default App;
--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,5 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+//import { composeWithDevTools } from "redux-devtools-extension";
+import { forbiddenWordsMiddleware } from "../middleware/index";
 import rootReducer from "../reducers/index";
-
-const store = createStore(rootReducer);
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware))
+);
 export default store;
